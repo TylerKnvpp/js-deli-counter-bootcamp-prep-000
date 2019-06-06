58d15897260083052ef8ff7001348f0154624572
tyler knapp <tgknapp11@gmail.com>
2019-06-06 14:37:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-function takeANumber(katzDeliLine, name) {
+const takeANumber = (katzDeliLine, name) => {
   katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
-    return "There is nobody waiting to be served!"
+const nowServing = katzDeliLine => {
+  let q = katzDeliLine.length
+  let name = katzDeliLine[0];
+  if (!q) {
+    return 'There is nobody waiting to be served!'
   } else {
-    var name = katzDeliLine[0];
     katzDeliLine.splice(0, 1);
     return `Currently serving ${name}.`;
   }
 }
 
-function currentLine(katzDeliLine) {
-    var line = []
-    if (katzDeliLine.length === 0) {
-      return "The line is currently empty."
+
+const currentLine = katzDeliLine => {
+    let line = []
+    let q = katzDeliLine.length
+
+    if (!q) {
+      return 'The line is currently empty.'
     } else {
-      for(var i = 0; i < katzDeliLine.length; i++) {
-        line += `${(i + 1)}. ${katzDeliLine[i]}, `
+      for(let i = 0; i < q; i++) {
+        line += `${(i + 1)}. ${katzDeliLine[i]},`
+
       }
       line = line.slice(0, line.length-2)
+        /////
       return `The line is currently: ${line}`
     }
 }
